ad78b8805622378dd29983a373ce82f5a95fb769
Niko Tanato <nmtanato@gmail.com>
2026-01-17 23:11:59 +0800

    [LOGIN SCREEN & SIGNUP SCREEN][UPDATE]

---
+++ screen/login-screen/login-screen.js
@@ -0,0 +1,8 @@
+// Select the Sign Up button
+const signupButton = document.querySelector(".SignUp-btn");
+
+// Add click event to redirect
+signupButton.addEventListener("click", () => {
+  // Redirect to signup-screen.html
+  window.location.href = "../signup-screen/signup-screen.html";
+});
